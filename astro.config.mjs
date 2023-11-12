47d076e527f6a5e5a129b3ca8bbb632ff0b09c92
proxima812 <mrzuc2015@yandex.ru>
2023-11-12 18:08:14 +0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
-
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
+import { SITE_URL } from './src/data/config'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
